88726c0cf53419a4a210491525d816346fd54ee9
satan sharma <satansharma7549@gmail.com>
2024-08-11 00:06:17 +0530

    backend user signup login and forgot password complete

---
+++ backend/utils/messages.js
@@ -0,0 +1,14 @@
+const messages = {
+  PROVIDE_ALL_CREDENTIAL: "Please provide all credential ",
+  USER_EXISTS: "User already exists",
+  SHORT_PASSWORD: "Password must be at least 7 characters.",
+  INVALID_USERNAME_PASSWORD: "Invalid username or password",
+  INVALID_EMAIL: "Invalid email address.",
+  PASSWORD_REQUIRED: "Password is required.",
+  TOKEN_EXPIRED: "Invalid or expired token",
+  SERVER_ERROR: "Server error.",
+  USER_NOT_FOUND: "User not found. You need to signup first.",
+  PRODUCT_NOT_FOUND: "Product not found",
+};
+
+module.exports = messages;
